f2902861762e8488a9dbdd8f673c8197394c91ef
Pavel Sudnik <wintry3@gmail.com>
2022-03-29 01:16:42 +0300

    Changed security manager interface

--- src/Authorization/security_manager.h
+++ src/Authorization/security_manager.h
@@ -1,8 +1,9 @@
 #ifndef _SECURITY_MANAGER_DREAM_TEAM_GUARD
 #define _SECURITY_MANAGER_DREAM_TEAM_GUARD
+#include <string>
 
-int VerifyLogin(char* login);
+bool CheckLoginCollision(const std::string& login);
 
-int VerifyPassword(char* password);
+int VerifyCredentials(const std::string& login, const std::string& password);
 
 #endif //_SECURITY_MANAGER_DREAM_TEAM_GUARD
