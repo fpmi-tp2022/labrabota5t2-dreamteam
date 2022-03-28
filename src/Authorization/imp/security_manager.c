2257fa942cd39a635c9182de18946fd635400b18
Pavel Sudnik <wintry3@gmail.com>
2022-03-28 23:49:53 +0300

    Add security manager

---
+++ src/Authorization/imp/security_manager.c
@@ -0,0 +1,10 @@
+#include "../security_manager.h"
+#include "../../Repositories/UserRepository.h"
+
+int VerifyLogin(char* login) {
+	return -1;
+}
+
+int VerifyPassword(char* password) {
+	return -1;
+}
---
+++ src/Authorization/security_manager.h
@@ -0,0 +1,8 @@
+#ifndef _SECURITY_MANAGER_DREAM_TEAM_GUARD
+#define _SECURITY_MANAGER_DREAM_TEAM_GUARD
+
+int VerifyLogin(char* login);
+
+int VerifyPassword(char* password);
+
+#endif //_SECURITY_MANAGER_DREAM_TEAM_GUARD
