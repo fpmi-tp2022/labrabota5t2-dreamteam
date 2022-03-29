41d42bc2f5dee728ca4ed910fe8b2809e29478bb
Pavel Sudnik <wintry3@gmail.com>
2022-03-29 11:35:05 +0300

    Inplement security manager

--- src/Authorization/imp/security_manager.cpp
+++ src/Authorization/imp/security_manager.cpp
@@ -3,18 +3,42 @@
 #include "../sha256.h"
 #include <iostream>
 
-std::string GetHash(const std::string& str);
+std::string GetHash(std::string str);
 
-bool CheckLoginCollision(const std::string& login) {
-	return false;
+Result CheckLoginCollision(const std::string& login) {
+	auto users = TryGetUserByLogin(login);
+	if (users.size() > 0) {
+		return Result::LOGIN_COLLISION;;
+	}
+	return Result::NO_ERROR;
 }
 
-int VerifyCredentials(const std::string& login, const std::string& password) {
+
+Result VerifyCredentials(const std::string& login, const std::string& password) {
+	auto users = TryGetUserByLogin(login);
+	if (users.size() != 0) {
+		return Result::LOGIN_COLLISION;
+	}
+
 	auto hash = GetHash(password);
+	if (hash != users[0].PasswordSigned) {
+		return Result::WRONG_PASSWORD;
+	}
 	std::cout << "Hash for " << password << '(' << hash << ')' << std::endl;
-	return -1;
+	return Result::NO_ERROR;
 }
 
-std::string GetHash(const std::string& str) {
+Result RegisterNewUser(const std::string& login, const std::string& password, Role role) {
+	Result result = Result::NO_ERROR;
+	if (FAILED(result = CheckLoginCollision(login))) {
+		return result;
+	}
+	auto hash = GetHash(password);
+
+	User user = {0, login, hash, role};
+	return (Result)(0 == AddUser(&user));
+}
+
+std::string GetHash(std::string str) {
 	return sha256(str);
 }
--- src/Authorization/security_manager.h
+++ src/Authorization/security_manager.h
@@ -1,9 +1,13 @@
 #ifndef _SECURITY_MANAGER_DREAM_TEAM_GUARD
 #define _SECURITY_MANAGER_DREAM_TEAM_GUARD
 #include <string>
+#include "../Models/Authorization/Role.h"
+#include "../Types/result_codes.h"
 
-bool CheckLoginCollision(const std::string& login);
+Result CheckLoginCollision(const std::string& login);
 
-int VerifyCredentials(const std::string& login, const std::string& password);
+Result VerifyCredentials(const std::string& login, const std::string& password);
+
+Result RegisterNewUser(const std::string& login, const std::string& password, Role role);
 
 #endif //_SECURITY_MANAGER_DREAM_TEAM_GUARD
