33c52478c60fadcd3f3820d2ab73011f51eb07d8
Artyom Shuneyko <artyomshuneyko@gmail.com>
2022-04-04 20:41:13 +0300

    Update TestFixture.cpp

--- test/TestFixture.cpp
+++ test/TestFixture.cpp
@@ -7,34 +7,8 @@
 
 class RepositoriesTests :public ::testing::Test {
 protected:
-	sqlite3* db;
-
-	static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
-		int i;
-		for (i = 0; i < argc; i++) {
-			printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-		}
-		printf("\n");
-		return 0;
-	}
-
-
 	void SetUp() override {
-		std::string sql, buff;
-		std::ifstream myfile("test.sql");
-		if (myfile.is_open())
-		{
-			while (getline(myfile, buff))
-			{
-				sql += buff;
-			}
-			myfile.close();
-		}
-
-		int rc = OpenConnection("test.db");
-		db = GetConnection();
-		char* zErrMsg = 0;
-		rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+		
 	}
 
 	void TearDown() override {
@@ -42,6 +16,6 @@ protected:
 	}
 };
 
-TEST(govno, zhopa) {
+TEST_F(RepositoriesTests, HorseRepository) {
 	auto a = GetHorsesWon();
 }
